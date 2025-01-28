ed9262529e70b3fd5e86f0c7218978db877bcc3c
matteosantillo <123595234+matteosantillo@users.noreply.github.com>
2025-01-28 15:06:08 -0500

    Add files via upload

---
+++ outputJS.js
@@ -0,0 +1,240 @@
+ {
+  type: "FeatureCollection",
+  name: "AL132021_pts",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021090818,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 8,
+        HHMM: "1800",
+        MSLP: 1006,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "TS",
+        INTENSITY: 35,
+        SS: 0,
+        LAT: 28.4,
+        LON: -86.8
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -86.8,
+          28.4
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021090900,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 9,
+        HHMM: "0000",
+        MSLP: 1001,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "TS",
+        INTENSITY: 45,
+        SS: 0,
+        LAT: 29.5,
+        LON: -85.4
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -85.4,
+          29.5
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021090906,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 9,
+        HHMM: "0600",
+        MSLP: 1001,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "TS",
+        INTENSITY: 35,
+        SS: 0,
+        LAT: 30.5,
+        LON: -83.9
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -83.9,
+          30.5
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021090912,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 9,
+        HHMM: "1200",
+        MSLP: 1005,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "TD",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 31.2,
+        LON: -81.8
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -81.8,
+          31.2
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021090918,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 9,
+        HHMM: "1800",
+        MSLP: 1005,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "TD",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 31.7,
+        LON: -79.4
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -79.4,
+          31.7
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021091000,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 10,
+        HHMM: "0000",
+        MSLP: 1005,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "LO",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 32.1,
+        LON: -76.8
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -76.8,
+          32.1
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021091006,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 10,
+        HHMM: "0600",
+        MSLP: 1005,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "LO",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 32.6,
+        LON: -74
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -74,
+          32.6
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021091012,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 10,
+        HHMM: "1200",
+        MSLP: 1005,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "LO",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 33.2,
+        LON: -71.8
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -71.8,
+          33.2
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        STORMNAME: "MINDY",
+        DTG: 2021091018,
+        YEAR: 2021,
+        MONTH: "09",
+        DAY: 10,
+        HHMM: "1800",
+        MSLP: 1006,
+        BASIN: "al",
+        STORMNUM: 13,
+        STORMTYPE: "LO",
+        INTENSITY: 30,
+        SS: 0,
+        LAT: 33.7,
+        LON: -69.8
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          -69.8,
+          33.7
+        ]
+      }
+    }
+  ]
+}
